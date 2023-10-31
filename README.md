a9af44ae912309341e500c48bc196b16a980f572
narayanab16 <narayanab16@gmail.com>
2023-10-30 20:20:38 -0500

    Client code Screen shot of the pods scale and shrink automatic

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # kubernetes-hpa-loadtest
 Kubernetes Horizontal Pod Autoscaling(HPA)  Load Test
+
+-  Prereq: Java 18 or higher, IntelliJ/Eclipse
+-  Spring Reactive WebClient - K8sLoadTestClient.java
+     - 20 Threads in Thread pool 
+     - 100K Iterations
+-  Screen shot of the pods scale and shrink automatic
+
+![img.png](img.png)   
---
+++ src/main/java/com/narayana/k8s/hpa/K8sLoadTestClient.java
@@ -0,0 +1,68 @@
+package com.narayana.k8s.hpa;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class K8sLoadTestClient {
+    private WebClient webClient;
+
+    public K8sLoadTestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+    private Mono<String> k8sHPALoadTestServer(K8sLoadTestClient client) {
+        return this.webClient.get().uri("/").accept(MediaType.APPLICATION_JSON)
+                .exchangeToMono(x ->
+                        x.statusCode().equals(HttpStatus.OK) ?
+                                x.bodyToMono(String.class) :
+                                Mono.justOrEmpty(null)
+                );
+    }
+
+    private static void k8sHPALoadTest(K8sLoadTestClient client, int ftps, int load_it) {
+        ExecutorService executorService = Executors.newFixedThreadPool(ftps);
+        List<Future<String>> responseList = new ArrayList<>();
+        try{
+            for (int i = 0; i < load_it; i++) {
+                Future<String> retValue = executorService.submit(() -> {
+                    Mono<String> stringMono = client.k8sHPALoadTestServer(client);
+                    String response = stringMono.block();
+                    return response;
+                });
+                responseList.add(retValue);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+        while (!executorService.isTerminated()){}
+
+        responseList.stream().forEach(x -> {
+            try {
+                String result = x.get();
+                System.out.println(" result : " + result);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        WebClient webClient =
+                WebClient.builder().baseUrl("http://localhost:8082").build();
+        int FIXED_THREAD_POOL_SIZE = 20;
+        int LOAD_ITERATIONS = 100000; // 100K iterations
+        K8sLoadTestClient client = new K8sLoadTestClient(webClient);
+        k8sHPALoadTest(client, FIXED_THREAD_POOL_SIZE, LOAD_ITERATIONS);
+    }
+}
