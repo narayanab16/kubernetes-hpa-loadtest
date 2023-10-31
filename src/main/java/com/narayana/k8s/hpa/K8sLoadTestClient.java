2602f98b371f6a33d7e199b6e2d9f8767edb0a3d
narayanab16 <narayanab16@gmail.com>
2023-10-31 09:21:35 -0500

    validate iterations from results counter

--- src/main/java/com/narayana/k8s/hpa/K8sLoadTestClient.java
+++ src/main/java/com/narayana/k8s/hpa/K8sLoadTestClient.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class K8sLoadTestClient {
     private WebClient webClient;
@@ -44,24 +45,26 @@ public class K8sLoadTestClient {
         }
         executorService.shutdown();
         while (!executorService.isTerminated()){}
-
+        AtomicLong counter = new AtomicLong();
         responseList.stream().forEach(x -> {
             try {
                 String result = x.get();
                 System.out.println(" result : " + result);
+                counter.incrementAndGet();
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             } catch (ExecutionException e) {
                 throw new RuntimeException(e);
             }
         });
+        System.out.println("Total requests : " + counter.get());
     }
 
     public static void main(String[] args) {
         WebClient webClient =
                 WebClient.builder().baseUrl("http://localhost:8082").build();
         int FIXED_THREAD_POOL_SIZE = 20;
-        int LOAD_ITERATIONS = 100000; // 100K iterations
+        int LOAD_ITERATIONS = 200000; // 100K iterations
         K8sLoadTestClient client = new K8sLoadTestClient(webClient);
         k8sHPALoadTest(client, FIXED_THREAD_POOL_SIZE, LOAD_ITERATIONS);
     }
